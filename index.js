270de7c96a3666462fa64055985a517bc2aaeaec
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2022-04-19 20:30:25 -0600

    Creación de una clase y modularización

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const daniel = new MissionCommander("Daniel")
+const beltran = new MissionCommander("Beltran")
+console.log(carlo.name)
+console.log(daniel.name)
+console.log(beltran.name)
